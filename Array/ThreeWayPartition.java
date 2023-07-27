6d2f96e97e82dd3c564e7a5cb79561cf65e00701
hrithik <hrithik086@gmail.com>
2023-07-27 23:55:53 +0530

    Three way partition

---
+++ Array/ThreeWayPartition.java
@@ -0,0 +1,9 @@
+/*
+ * question link - https://practice.geeksforgeeks.org/problems/three-way-partitioning/1
+ */
+
+public class ThreeWayPartition {
+    public static void main(String args[]){
+
+    }
+}
