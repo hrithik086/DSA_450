882a336a94d92c7dab9c5d62296c08347254e1d2
hrithik <hrithik086@gmail.com>
2024-01-06 15:08:08 +0530

    one of the optimized way to find the kth smallest element in a row and column wise sorted matrix implemented using binary search

--- Matrix/KthSmallestElement.java
+++ Matrix/KthSmallestElement.java
@@ -1,233 +1,113 @@
 /*
  * Question link - https://practice.geeksforgeeks.org/problems/kth-element-in-matrix/1
+ * Explanation Video link of Similar question - https://www.youtube.com/watch?v=aNIfGl1ZKQE&t=1004s&ab_channel=ShashwatTiwari
+ * Explanation video for this solution - https://www.youtube.com/watch?v=Q9wXgdxJq48&t=1178s&ab_channel=takeUforward
  */
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 
 public class KthSmallestElement {
     public static void main(String args[]){
-        int mat[][] = {{16, 28, 60, 64},
-                        {22, 41, 63, 91},
-                        {27, 50, 87, 93},
-                        {36, 78, 87, 94 }
-                    };
-        // System.out.println(kthSmallest(mat, mat.length, 3));
-        System.out.println(kthSmallestElement(mat, 3));
+        // int mat[][] = {{16, 28, 60, 64},
+        //                 {22, 41, 63, 91},
+        //                 {27, 50, 87, 93},
+        //                 {36, 78, 87, 94 }
+        //             };
+        int mat[][] = {
+            {7 ,17 ,27, 36, 38},
+            {14, 23, 35, 38, 43},
+            {19, 26, 42, 49, 50},
+            {23, 33, 48, 52, 53},
+            {30, 40, 52, 56, 64}
+        };
+
+        System.out.println(kthSmallestElement(mat, 13));
 
     }
 
+    public static int kthSmallestElement(int mat[][], int index){
+        int start=mat[0][0];
+        int end=mat[mat.length-1][mat[mat.length-1].length-1];
+        int mid=Integer.MIN_VALUE;
+        int value=Integer.MIN_VALUE;
+        int noOfSmallerElements;
 
-    public static int kthSmallestElement(int mat[][], int k){
-        ArrayList<Integer> minHeap = new ArrayList<Integer>();
-        int kthMinElement = 0;
-        
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                minHeap.add(mat[i][j]);
-                buildMinHeap(minHeap);
+        while(start <= end){
+            mid = (start+end)/2;
+            noOfSmallerElements = noOfElementsLessThanValueInMatrix(mat, mid);
+
+            //it should capture the smallest value present in the sorted matrix
+            //such that noOfElements which is <= this value in the matrix
+            //this noOfElement should be just greater or equal to the index
+            value = noOfSmallerElements >= index ? mid : value;
+
+            if(noOfSmallerElements > index){
+                end = mid-1;
             }
-        }
-
-        for(int i=0; i<k;i++){
-            kthMinElement = getMinElement(minHeap);
-        }
-
-        return kthMinElement;
-    }
-
-    public static void buildMinHeap(ArrayList<Integer> list){
-
-        if(list.size()==0){
-            return;
-        }
-
-        int currentIndex = list.size()-1;
-        int parentIndex = parentIndex(currentIndex);
-        int currentNodeValue = list.get(currentIndex);
-
-        while(parentIndex>=0 && list.get(parentIndex)>currentNodeValue){
-            swap(list, currentIndex, parentIndex);
-            currentIndex=parentIndex;
-            parentIndex=parentIndex(currentIndex);
-        }
-    }
-
-    public static int getMinElement(ArrayList<Integer> list){
-        if(list.size()==0){
-            return Integer.MAX_VALUE;
-        }
-
-        int min=list.get(0);
-        list.set(0, list.get(list.size()-1));
-        list.remove(list.size()-1);
-        if(list.size()>0){
-            heapify(list, 0);
-        }
-
-        return min;
-    }
-
-    public static void heapify(ArrayList<Integer> list, int index){
-        int parentIndex = index;
-        int leftChildIndex;
-        int rightChildIndex;
-
-        int parent;
-        int leftChild;
-        int rightChild;
-        
-        while(parentIndex<list.size()){
-            parent=list.get(parentIndex);
-            leftChildIndex = leftChildIndex(parentIndex);
-            rightChildIndex = rightChildIndex(parentIndex);
-
-            if(leftChildIndex < list.size() && rightChildIndex < list.size()){
-                leftChild = list.get(leftChildIndex);
-                rightChild = list.get(rightChildIndex);
-
-                if(leftChild <= rightChild){
-                    if(leftChild < parent){
-                        swap(list, leftChildIndex, parentIndex);
-                        parentIndex = leftChildIndex;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                else{
-                    if(rightChild < parent){
-                        swap(list, rightChildIndex, parentIndex);
-                        parentIndex = rightChildIndex;
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
-            else if(leftChildIndex < list.size() && rightChildIndex == list.size()){
-                leftChild=list.get(leftChildIndex);
-                if(leftChild < parent){
-                    swap(list, leftChildIndex, parentIndex);
-                    parentIndex = leftChildIndex;
-                }
+            else if(noOfSmallerElements < index){
+                start = mid+1;
             }
             else{
+                end = mid-1;
+            }
+        }
+
+        return value;
+    }
+
+    public static int noOfElementsLessThanValueInMatrix(int mat[][], int value){
+        int count=0;
+        int tempCount=0;
+        for(int i=0;i<mat.length;i++){
+            tempCount=noOfElementsLessThanValueInArray(mat[i], value);
+            count+=tempCount;
+            if(tempCount==0){
                 break;
             }
         }
+
+        return count;
     }
 
-    public static int parentIndex(int childIndex){
-        int parent;
-        if(childIndex%2 == 0){
-            parent = childIndex/2 -1;
-        }
-        else{
-            parent = childIndex/2;
-        }
+    public static int noOfElementsLessThanValueInArray(int arr[], int value){
 
-        return parent;
-    }
-
-    public static int leftChildIndex(int parentIndex){
-        return (2*parentIndex)+1;
-    }
-
-    public static int rightChildIndex(int parentIndex){
-        return (2*parentIndex)+2;
-    }
-
-    public static void swap(ArrayList<Integer> list, int index1, int index2){
-        list.set(index1, list.get(index1)+list.get(index2));
-        list.set(index2, list.get(index1)-list.get(index2));
-        list.set(index1, list.get(index1)-list.get(index2));
-    }
-
-
-
-    //-------------------------------------------------------------------------------------------------------
-    //this is my approach we can still optimize this
-    public static int kthSmallest(int[][]mat,int n,int k)
-	{
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        int counter=0;
-        
-        for(int i=0; i<mat.length; i++){
-            for(int j=0; j<mat[0].length; j++){
-                
-                if(counter<k){
-                    binarySearchAndAdd(linkedList, mat[i][j], true);
-                    counter++;
-                }
-                else{
-                    binarySearchAndAdd(linkedList, mat[i][j], false);
-                }
-                
-            }
-        }
-
-        return linkedList.get(linkedList.size()-1);
-    }
-
-    public static int binarySearchAndAdd(LinkedList<Integer> linkedList, int item, boolean expand){
-        int insertIndex = binarySearch(linkedList, item);
-
-        if(expand){
-            if(insertIndex == linkedList.size()){
-                linkedList.add(item);
-            }
-            else{
-                linkedList.add(insertIndex, item);
-            }
-        }
-        else{
-            linkedList.add(insertIndex, item);
-            linkedList.removeLast();
-        }
-        
-        return 0;
-    }
-    
-    public static int binarySearch(LinkedList<Integer> linkedList, int item){
+        int index=Integer.MIN_VALUE;
+        int tempIndex;
         int start=0;
-        int end=linkedList.size()-1;
+        int end=0;
         int mid=0;
 
-        if(linkedList.size()!=0){
-            while(start<end){
-                mid=(start+end)/2;
-                if(linkedList.get(mid)==item){
-                    return mid;
-                }
-                else if(linkedList.get(mid)<item){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
 
-            if(start==end){
-                if(linkedList.get(start)==item){
-                    return start;
-                }
-                return linkedList.get(start) > item ? start : start+1;
-            }
-            else if(start != end){
-                if(start>=0 && start<linkedList.size() && linkedList.get(start) >= item){
-                    return start;
-                }
-                else if(end>=0 && end<linkedList.size() && linkedList.get(end) >= item){
-                    return end;
-                }
-            }
-
-            return -1;
+        //get upper bound index
+        if(value >= arr[arr.length-1]){
+            return arr.length;
+        }
+        else if(value < arr[0]){
+            return 0;
         }
         else{
-            return 0;
-        } 
+            start=0;
+            end=arr.length-1;
+
+            while(start<=end){
+                mid=(start+end)/2;
+
+                if(arr[mid]>value){
+                    tempIndex=mid;
+                    //to pick the first occurence of that element in the sorted array
+                    while(tempIndex>=0 && arr[tempIndex]==arr[mid]){
+                        index=tempIndex;
+                        tempIndex--;
+                    }
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+
+            }
+
+        }
+
+        return index;
     }
+    
 }
